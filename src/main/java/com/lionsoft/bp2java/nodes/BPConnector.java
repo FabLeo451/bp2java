96a3509c9b5d286e6bce93b88c1fbfb2d1f8fff9
FabLeo451 <fab.leo@gmail.com>
2020-06-26 14:37:34 +0200

    Connector nullable

--- src/main/java/com/lionsoft/bp2java/nodes/BPConnector.java
+++ src/main/java/com/lionsoft/bp2java/nodes/BPConnector.java
@@ -8,7 +8,7 @@ class BPConnector {
   // Direction
   public final static int INPUT = 0;
   public final static int OUTPUT = 1;
-  
+
   // Type
   /*
   public final static int EXEC = 0;
@@ -19,7 +19,7 @@ class BPConnector {
   public final static int JSON = 5;
   public final static int POINTER = 6;
   */
-  
+
   // Dimensions
   /*
   public final static int SCALAR = 0;
@@ -41,23 +41,24 @@ class BPConnector {
   Reference references;
   BPType type; // This is data type
   //Boolean mustConnect;
-  
+  Boolean nullable = false; // If true, can be disconnected
+
   /*int must_connect;
   int not_null;*/
   //int flags;
-  
+
   /*
    * connectedTo:
    * - For exec: exec connector of next node
    * - For data: data connector of previous node
    */
   BPConnector connectedTo;
-  
+
   int referenceId;
   //BPVariable *references;
-  
+
   //static String strTypes[] = {"_exec_", "int", "float", "String", "Boolean"};
-   
+
   public BPConnector() {
     exec = false;
     value = null;
@@ -66,10 +67,10 @@ class BPConnector {
     references = null;
     //mustConnect = false;
   }
-   
+
   public BPConnector(int direction, JSONObject jc) {
     this();
-    
+
     setId(((Long) jc.get("id")).intValue());
     setLabel((String) jc.get("label"));
     //setDataType(((Long) jc.get("dataType")).intValue());
@@ -79,26 +80,26 @@ class BPConnector {
     setDataTypeName((String) jc.get("dataType"));
     setExec((Boolean) jc.get("exec"));
     setDimensions(((Long) jc.get("dimensions")).intValue());
-    
+
     //type = new BPType((String) jc.get("dataTypeName"));
     type = new BPType((String) jc.get("dataType"));
-    
-    /*if (jc.containsKey("must_connect"))
-      setMustConnect((Boolean)jc.get("must_connect"));*/
+
+    if (jc.containsKey("nullable"))
+      setNullable((Boolean)jc.get("nullable"));
 
     setDirection(direction);
-    
+
     if (jc.containsKey("reference_id"))
       setReferenceId(((Long)jc.get("reference_id")).intValue());
-    
+
     if (jc.containsKey("java")) {
       JSONObject java = (JSONObject) jc.get("java");
-      
+
       if (java.containsKey("references")) {
         String varName = null;
-        
+
         JSONObject jref = (JSONObject) java.get("references");
-        
+
         if (jref.containsKey("input")) {
           // Reference an input connector
           references = new Reference(((Long)jref.get("input")).intValue());
@@ -117,12 +118,12 @@ class BPConnector {
         else {
           System.out.println("'references' without target object");
         }
-        
+
         //setFixedOutput(varName);
         //System.out.println(label+" references "+references.toString());
       }
     }
-    
+
     if (jc.containsKey("value")) {
       /*
       switch (dataType) {
@@ -145,7 +146,7 @@ class BPConnector {
           break;
       }
       */
-      
+
       if (dataTypeName.equals("Integer") || dataTypeName.equals("Boolean"))
         value = (Object) jc.get("value");
       else if (dataTypeName.equals("Double"))
@@ -153,19 +154,19 @@ class BPConnector {
       else if (dataTypeName.equals("String"))
         value = (Object) ((String)jc.get("value")).replace("\\","\\\\").replace("\n","\\n").replace("\"","\\\"");
       else {
-      
+
       }
     }
   }
-  
+
   public void createReferenceLocalVariable (String decl, String name) {
     String varName = null;
-    
+
     varName = "_conn_"+name+"_"+id;
     references = new Reference(decl, varName);
     setFixedOutput(varName);
   }
-  
+
   public void createReferenceLocalVariable (JSONObject jref) {
     createReferenceLocalVariable ((String)jref.get("type"), (String)jref.get("name"));
   }
@@ -177,36 +178,36 @@ class BPConnector {
   public void setReference(Reference r) {
     references = r;
   }
-  
+
   public void setId (int id) {
     this.id = id;
   }
-  
+
   public int getId () {
     return (id);
   }
-/*  
+/*
   public void setDataType (int type) {
     this.dataType = type;
   }
-  
+
   public int getDataType () {
     return (dataType);
   }
-*/  
-  
+*/
+
   public String getDataTypeName () {
     return (dataTypeName);
   }
-  
+
   public void setDataTypeName (String name) {
     this.dataTypeName = name;
   }
-/*  
+/*
   public void setPinType (int type) {
     this.pinType = type;
   }
-  
+
   public int getPinType () {
     return (pinType);
   }
@@ -214,7 +215,7 @@ class BPConnector {
   public void setPinTypeName (String name) {
     this.pinTypeName = name;
   }
-  
+
   public String getPinTypeName () {
     return (pinTypeName);
   }
@@ -222,60 +223,59 @@ class BPConnector {
   public void setLabel (String label) {
     this.label = label;
   }
-  
+
   public String getLabel () {
     return (label);
   }
-  
+
   public void setDirection (int direction) {
     this.direction = direction;
   }
-  
+
   public int getDirection () {
     return (direction);
   }
-  
+
   public void setDimensions(int d) {
     dimensions = d;
   }
-  
+
   public int getDimensions() {
     return (dimensions);
   }
-    
+
   public void setExec (Boolean exec) {
     this.exec = exec;
   }
-  
+
   public Boolean getExec () {
     return (exec);
   }
 
   public boolean mustBeConnected() {
-    return (!getExec() && getValue() == null);
+    return (!getExec() && getValue() == null && !getNullable());
   }
 
-  /*
-  public void setMustConnect (Boolean b) {
-    this.mustConnect = b;
+  public void setNullable (Boolean b) {
+    this.nullable = b;
   }
-  
-  public Boolean getMustConnect () {
-    return (this.mustConnect);
-  }*/
-  
+
+  public Boolean getNullable () {
+    return (this.nullable);
+  }
+
   public void setNode (BPNode node) {
     this.node = node;
   }
-  
+
   public BPNode getNode () {
     return (node);
   }
-  
+
   public void setReferenceId (int id) {
     this.referenceId = id;
   }
-  
+
   public int getReferenceId () {
     return (referenceId);
   }
@@ -295,30 +295,30 @@ class BPConnector {
   public boolean isConnected() {
     return (connectedTo != null);
   }
-  
+
   public void setFixedOutput(String s) {
     fixedOutput = s;
   }
-  
+
   public String getFixedOutput() {
     return (fixedOutput);
   }
-  
+
   public Reference getReference() {
     return (references);
   }
-  
+
   public void setValue(Object v) {
     value = v;
   }
-  
+
   public Object getValue() {
     return(value);
   }
 
   public String getValueAsString() {
-    String result = "";
-    
+    String result = "null";
+
     if (isConnected() && direction == BPConnector.INPUT) {
       // This node. Input pin connected: go backwards
       result = getConnected().getValueAsString();
@@ -326,7 +326,7 @@ class BPConnector {
     else {
       if (fixedOutput != null) {
         return (fixedOutput);
-      } 
+      }
       else if (references != null) {
         if (references.getRefType() == Reference.INPUT) {
           result = getNode().getInputConnector(references.inputConnector).getValueAsString();
@@ -359,8 +359,7 @@ class BPConnector {
         }
       }
     }
-    
+
     return (result);
   }
 };
-
